ce24ab3743c11e1d575b4a9173aecceba202ac02
Jose Beltre <552006jose@gmail.com>
2026-01-08 14:50:16 -0400

    aded OG IMAGE and metatags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,22 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
         { rel: 'manifest', href: '/favicon/site.webmanifest' }
+      ],
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Sistema para gestión de Holypoints de los jóvenes de Mansedumbre 2' },
+        { name: 'author', content: 'José L. Beltre' },
+        { name: 'keywords', content: 'Jóvenes, Administra, Holypoints, Mansedumbre 2' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: 'HolyPoints' },
+        { property: 'og:image', content: '/og/default.png' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:title', content: 'HolyPoints' },
+        { property: 'twitter:description', content: 'Sistema para gestión de Holypoints de los jóvenes de Mansedumbre 2' },
+        { property: 'twitter:image', content: '/og/default.png' }
       ]
     }
   }
