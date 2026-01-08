d29cca9536d4f9f0f2609a7bd3ea3001fb1647ce
Jose Beltre <552006jose@gmail.com>
2026-01-08 00:44:14 -0400

    Implemented full home design

---
+++ app/components/KpiCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  stat: number | string,
+  text: string,
+  className?: string
+}>()
+</script>
+<template>
+  <div :class="['flex w-full h-36 bg-secondary-300 rounded-2xl p-4 overflow-hidden', className]">
+    <div class="w-1/2 relative">
+      <div class="absolute top-2 -left-14 -rotate-12">
+        <slot name="icon" />
+      </div>
+    </div>
+    <div class="w-56 flex flex-col justify-center">
+      <h2 class="font-black text-6xl text-white leading-12">{{ stat }}</h2>
+      <p class="font-extrabold text-2xl leading-5">{{ text }}</p>
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,50 +6,40 @@
     </header>
     <!-- Texto de bienvenido dinamico -->
     <h1 class="text-4xl font-bold tracking-tight leading-5">Bienvenido</h1>
-    <p class="font-light ">Aquí un resumen de tus jovenes 👇</p>
+    <p class="font-light text-black/60">Aquí un resumen de tus jovenes <span class="text-white">👇</span></p>
     <main class="grid gap-4 mt-3">
       <!-- Cards de resumen -->
-      <div>
-        <div class="flex w-full h-36 bg-secondary-300 rounded-2xl p-4 overflow-hidden">
-          <div class="w-1/2 relative">
-            <div class="absolute -top-2 -left-10">
-              <IconGem :size="192" class="-rotate-12 absolute" stroke-width=".8" fill="#ffffff" />
-              <IconGem :size="192" class="-rotate-12 absolute" stroke-width=".8" />
-            </div>
-          </div>
-          <div class="w-56 flex flex-col justify-center">
-            <h2 class="font-black text-6xl text-white leading-12">3000</h2>
-            <p class="font-extrabold text-2xl leading-5">Holypoints Reclamados</p>
-          </div>
-        </div>
+      <div class="grid gap-4">
+        <KpiCard :stat="3000" text="Holypoints Reclamados">
+          <template #icon>
+            <IconGem :size="192" class="absolute" stroke-width=".8" fill="#ffffff" />
+            <IconGem :size="192" class="absolute" stroke-width=".8" />
+          </template>
+        </KpiCard>
+        <KpiCard :stat="12" text="Jovenes Participando" class-name="!bg-primary-600">
+          <template #icon>
+            <IconUserRound :size="192" class="absolute" stroke-width=".8" fill="#ffffff" />
+          </template>
+        </KpiCard>
       </div>
       <!-- Atajos -->
-      <div class="border-2 rounded-2xl border-black/20 p-4 pt-2 shadow-md">
+      <div class="rounded-box">
         <h2 class="text-primary-600 font-bold text-2xl mb-2">Atajos</h2>
         <div class="grid gap-2">
-          <button class="flex p-2 py-3 justify-center border-2 border-primary-600 bg-primary-600/30 rounded-xl text-primary-600 font-medium relative overflow-hidden">
-            <IconGem class="absolute top-0 left-2 size-16 -rotate-10" stroke-width="1.2" />
+          <button class="shortcut-btn">
+            <IconGem stroke-width="1.2" />
             Sumar Holypoints
           </button>
-          <button class="flex p-2 py-3 justify-center border-2 border-primary-600 bg-primary-600/30 rounded-xl text-primary-600 font-medium relative overflow-hidden">
-            <IconUserRoundPlus class="absolute top-0 left-2 size-16 -rotate-10" stroke-width="1.2" />
+          <button class="shortcut-btn">
+            <IconUserRoundPlus stroke-width="1.2" />
             Registrar Joven
           </button>
         </div>
       </div>
       <!-- Lider en Holypoints -->
-      <div>
-        <h2>Lider en Holypoints</h2>
-        <div>
-          <div>
-            <h2>1</h2>
-            <p>Noel Rodriguez</p>
-            <h2>
-              421
-              <IconGem />
-            </h2>
-          </div>
-        </div>
+      <div class="rounded-box">
+        <h2 class="text-secondary-500 font-bold text-2xl mb-2">Lider en Holypoints</h2>
+        <TopYouth name="Noel Rodriguez" :points="421" />
       </div>
     </main>
   </div>
