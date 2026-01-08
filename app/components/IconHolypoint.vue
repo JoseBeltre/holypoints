4e0f3e7f3bb958490fe374854ce5a8437ab677ea
Jose Beltre <552006jose@gmail.com>
2026-01-08 16:50:46 -0400

    Create IconHolypoint component

---
+++ app/components/IconHolypoint.vue
@@ -0,0 +1,13 @@
+
+<script lang="ts" setup>
+defineProps<{
+  size?: number,
+  strokeWidth?: number
+}>()
+</script>
+<template>
+  <div class="relative size-5 inline-block">
+    <IconGem class="-rotate-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-black" :size="size" fill="#fbbf24" :stroke-width="strokeWidth" />
+    <IconGem class="-rotate-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-black" :size="size" :stroke-width="strokeWidth" />
+  </div>
+</template>
--- app/components/TopYouth.vue
+++ app/components/TopYouth.vue
@@ -5,15 +5,12 @@ defineProps<{
 }>()
 </script>
 <template>
-  <div class="flex justify-between items-center border-2 border-secondary-500 bg-secondary-100 rounded-xl p-4 text-xl relative cursor-pointer">
+  <NuxtLink to="/ranking" class="flex justify-between items-center border-2 border-secondary-500 bg-secondary-100 rounded-xl p-4 text-xl relative cursor-pointer">
     <h2 class="text-[40px] font-jolly text-secondary-500 absolute -top-6 -left-3 rotate-12">#1</h2>
     <p class="text-secondary-600 font-medium ps-2">{{ name }}</p>
     <div class="font-bold flex items-center gap-1 ">
       <h2>{{ points }}</h2>
-      <div class="relative size-5">
-        <IconGem class="-rotate-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2" :size="20" fill="#fbbf24" stroke-width="1.5" />
-        <IconGem class="-rotate-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2" :size="20" stroke-width="1.5" />
-      </div>
+      <IconHolypoint :size="20" :strokeWidth="1.5" />
     </div>
-  </div>
+  </NuxtLink>
 </template>
