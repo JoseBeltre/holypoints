beac32468e5ee7c8d812640cebe249c2f1f349b3
Jose Beltre <552006jose@gmail.com>
2026-01-08 23:56:36 -0400

    implement NotLogged design for Reward page components

--- app/components/SmallSummaryCard.vue
+++ app/components/SmallSummaryCard.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
 const props = defineProps<{
-  stat: number
+  stat: number,
+  isLogged?: boolean
 }>()
+
 </script>
 <template>
-  <div class="w-1/3 flex justify-center items-center bg-primary-600 text-white rounded-xl p-3 overflow-hidden relative">
-    <div class="size-20 absolute top-0 left-0 text-primary-700 -rotate-12">
+  <div :class="['flex justify-center items-center bg-primary-600 text-white rounded-xl overflow-hidden relative', !isLogged ? 'flex-1 p-1' : 'w-1/3 p-2']">
+    <div :class="['size-20 absolute top-0 left-0 text-primary-700 -rotate-12', !isLogged ? 'left-3 size-28' : '']">
       <slot />
     </div>
-    <h2 class="text-4xl font-extrabold ps-10">{{ stat }}</h2>
+    <div :class="['flex justify-center items-center ps-10', !isLogged ? 'gap-2' : '']">
+      <h2 :class="['font-extrabold', !isLogged ? 'text-7xl' : 'text-5xl']">{{ stat }}</h2>
+      <p v-if="!isLogged" class="font-extrabold text-3xl leading-6">Premios <br>Canjeables</p>
+    </div>
   </div>
 </template>
--- app/pages/youth.vue
+++ app/pages/youth.vue
@@ -6,7 +6,7 @@
     </header>
     <main class="grid gap-3">
       <section class="flex gap-3 w-full">
-        <SmallSummaryCard :stat="12">
+        <SmallSummaryCard isLogged :stat="12">
             <IconUserRound class="size-full" fill="#ffffff" stroke-width="1" />
         </SmallSummaryCard>
         <AddButton>Registrar Joven</AddButton>
