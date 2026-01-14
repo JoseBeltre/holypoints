981529f33cd8399426cf990e54c3ade03847a62e
Jose Beltre <552006jose@gmail.com>
2026-01-14 01:44:40 -0400

    implement Ranking page design for mobile
    - TODO: Desktop design

---
+++ app/components/RankingItem.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+defineProps<{
+  name: string,
+  points: number,
+  img?: string,
+  position: number
+}>();
+</script>
+<template>
+  <div v-if="position <= 3" :class="[
+    'winners-base',
+    position === 1 && 'from-fromGold to-toGold mb-5',
+    position === 2 && 'from-fromSilver to-toSilver mb-5',
+    position === 3 && 'from-fromBronze to-toBronze'
+  ]">
+    <div :class="[
+      'winners-img',
+      position === 1 && 'border-fromGold',
+      position === 2 && 'border-fromSilver',
+      position === 3 && 'border-fromBronze'
+    ]">
+      <img v-if="img" class="size-full object-cover" :src="img" alt="">
+      <DefaultImg v-else />
+    </div>
+    <h3 :class="[
+      position === 1 && 'text-nameGold',
+      position === 2 && 'text-nameSilver',
+      position === 3 && 'text-nameBronze'
+    ]">{{ name }}</h3>
+    <div :class="[
+      position === 1 && 'text-pointsGold',
+      position === 2 && 'text-pointsSilver',
+      position === 3 && 'text-pointsBronze'
+    ]">
+      <h3>{{ points }}</h3>
+      <p>Holypoints</p>
+    </div>
+  </div>
+  <div v-else class="ranking-base-wrapper mb-3">
+      <p class="text-lg">#{{ position }}</p>
+      <div class="ranking-base">
+        <div class="ranking-img">
+          <DefaultImg />
+        </div>
+        <h3>{{ name }}</h3>
+        <div>
+          <h3>{{ points }}</h3>
+          <p>Holypoints</p>
+        </div>
+      </div>
+    </div>
+</template>
--- app/pages/ranking.vue
+++ app/pages/ranking.vue
@@ -2,10 +2,39 @@
 useHead({
   title: 'Tabla de Puntuaciones - Holypoints'
 })
+// datos de ejemplo
+const users = [
+  {
+    name: "Nemesis Ramos",
+    points: 1000,
+  },
+  {
+    name: "Juan Ramos",
+    points: 800,
+  },
+  {
+    name: "Jose Beltre",
+    points: 700,
+  },
+  {
+    name: "Mario Ramirez",
+    points: 600,
+  },
+  {
+    name: "Juan Ramirez",
+    points: 500,
+  },
+]
 </script>
 <template>
-  <div>
+  <header class="mb-6">
     <h1 class="page-title">Tabla de Puntuaciones</h1>
     <p class="page-subtitle">Líderes en Holypoints</p>
-  </div>
+  </header>
+  <main class="flex flex-col">
+    <template v-for="(user, index) in users">
+      <RankingItem :name="user.name" :points="user.points" :position="index + 1" />
+      <div v-if="index === 2" class="h-px w-full bg-black/20 my-5"></div>
+    </template>
+  </main>
 </template>
