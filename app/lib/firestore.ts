f5bf23dc7d2b0c8f95da9b8329c21daf3c3f5192
Jose Beltre <552006jose@gmail.com>
2026-01-19 01:28:34 -0400

    Add firebase dependencie for the backend

---
+++ app/lib/firestore.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+// import { getAnalytics } from "firebase/analytics"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdwYEoJ9PWky4z00IPyK9wO_K-hPTGlLQ",
+  authDomain: "holypoints-m2.firebaseapp.com",
+  projectId: "holypoints-m2",
+  storageBucket: "holypoints-m2.firebasestorage.app",
+  messagingSenderId: "452948509066",
+  appId: "1:452948509066:web:afba2ae15e900ce187c873",
+  measurementId: "G-VKDPLKB1SH"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(firebaseApp)
+
+export const auth = getAuth(firebaseApp)
