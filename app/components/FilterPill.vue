7d43219a1c6009448540a017fd3a7b15bb806c05
Jose Beltre <552006jose@gmail.com>
2026-01-18 23:24:38 -0400

    Implement Logs page initial part
    - TODO: Finish logs page, create log elements

---
+++ app/components/FilterPill.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="flex gap-2 bg-gray-200 items-center p-1 px-3 rounded-full text-sm">
+    <slot></slot>
+  </button>
+</template>
---
+++ app/pages/settings/logs.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'sub-page'
+})
+useHead({
+  title: 'Registro de Acciones - Holypoints'
+})
+</script>
+<template>
+  <div>
+    <header class="mb-4">
+      <h1 class="page-title mb-1">Registro</h1>
+      <p class="page-subtitle">Este es un registro de todas las acciones relizadas.</p>
+    </header>
+    <main>
+      <section class="flex gap-2">
+        <FilterPill>
+          <IconGem :size="18" />
+          Puntos
+        </FilterPill>
+        <FilterPill>
+          <IconGift :size="18" />
+          Premios
+        </FilterPill>
+        <FilterPill>
+          <IconUsersRound :size="18" />
+          Jovenes
+        </FilterPill>
+      </section>
+    </main>
+  </div>
+</template>
