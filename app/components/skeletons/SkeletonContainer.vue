c16f83bd3ac3ab3d6fb8f1480a895bff5386b383
José Beltre <552006jose@gmail.com>
2026-01-22 14:09:35 -0400

    Add skeleton components for loading states

---
+++ app/components/skeletons/SkeletonContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-100 w-full flex-1 animate-pulse">
+    <slot />
+  </div>
+</template>
---
+++ app/components/skeletons/YouthCardSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-gray-100 h-16 animate-pulse w-full rounded-xl flex justify-between items-center gap-4 p-3">
+    <div class="bg-gray-200 rounded-full size-12" />
+    <div class="flex-1">
+      <div class="w-20 h-4 rounded-full bg-gray-200 mb-2" />
+      <div class="w-12 h-2 rounded-full bg-gray-200" />
+    </div>
+    <div class="bg-gray-200 h-full w-38 rounded-lg" />
+  </div>
+</template>
